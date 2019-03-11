6229ced3cccc2a838e7aeea3da18aeabf21841b7
mhocke <mathis.hocke@hubur.org>
2019-03-11 12:01:23 +0100

    load data as list of contours, calculate center

--- fish_pose_pca/load_contours.py
+++ fish_pose_pca/load_contours.py
@@ -1,37 +1,29 @@
 """utility for loading contour points from the biotracker export."""
 import json
-from typing import Dict, List
-
-
-class ContourPoint:
-    def __init__(self, data: Dict):
-        self.x = data["x"]
-        self.y = data["y"]
+from typing import List
+import numpy as np
+import cv2
 
 
 class Contour:
     def __init__(self, data: List):
-        self.points = [ContourPoint(d) for d in data]
-
-    def min_x(self):
-        return min([p.x for p in self.points])
-
-    def min_y(self):
-        return min([p.y for p in self.points])
-
-    def max_x(self):
-        return max([p.x for p in self.points])
-
-    def max_y(self):
-        return max([p.y for p in self.points])
+        self.xs = np.array([d["x"] for d in data])
+        self.ys = np.array([d["y"] for d in data])
+        contour = cv2.UMat(np.array([np.array([np.array([x, y])]) for x, y in zip(self.xs, self.ys)]))
+        moments = cv2.moments(contour)
+        self.c_x = int(moments["m10"] / moments["m00"])
+        self.c_y = int(moments["m01"] / moments["m00"])
 
 
-class Frame:
-    def __init__(self, line: str):
-        self.contours = [Contour(c) for c in json.loads(line)]
+def _get_contours(line: str):
+    return [Contour(c) for c in json.loads(line)]
 
 
-def load_contours(path: str) -> [Frame]:
+def load_contours(path: str):
     with open(path) as data:
         lines = data.readlines()
-    return [Frame(line) for line in lines]
+    contours = []
+    for line in lines:
+        for data in json.loads(line):
+            contours.append(Contour(data))
+    return contours
