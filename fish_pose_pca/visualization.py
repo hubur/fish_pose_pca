8ad8554356fce0b69e94ad2b1046d97db89f198e
opmueller <pascal@pascal-mueller.info>
2019-03-13 18:39:26 +0100

    tools for visualizing pca output

--- fish_pose_pca/visualization.py
+++ fish_pose_pca/visualization.py
@@ -2,7 +2,12 @@
 from pathlib import Path
 from typing import Callable, List
 import matplotlib.pyplot as plt
-
+from sklearn.decomposition import PCA
+import numpy as np
+import copy
+import statistics
+import pca
+import fishy
 
 def save_video_frames(parameters: List, fun: Callable, out_folder: str):
     """calls fun for each item in parameters and saves the
@@ -15,3 +20,100 @@ def save_video_frames(parameters: List, fun: Callable, out_folder: str):
         plt.imshow(img, cmap="gray")
         frame_name = f"{i}.png"
         plt.savefig(str(out_path / frame_name))
+
+
+def print_reconstruction_compare(pca_object, pca_input, transformed_fishes, n_components, fish_index):
+    fig, axes = plt.subplots(ncols=2, nrows=1, sharey=True)
+    old, new = fishy.get_fish_on_canvas(xy_array=pca_input[fish_index]), fishy.get_fish_on_canvas(
+        xy_array=pca_object.inverse_transform(transformed_fishes[fish_index]))
+    fig.set_figwidth(val=15)
+    fig.set_figheight(val=18)
+    axes[0].imshow(old, cmap="gray")
+    axes[1].imshow(new, cmap="gray")
+    axes[0].set_title("original")
+    axes[1].set_title(f"reconstructed from {n_components} Principal Components")
+    plt.show()
+
+
+def print_min_max_fishes(pca_object, pca_input, transformed_fishes, n_components, n_fishes, principal_component_index):
+    sorted_fishes = transformed_fishes[:, principal_component_index].argsort()
+    # plt.hist(transformed_fishes[:,principal_component_index],bins=30)
+    plt.show()
+    min_fishes = sorted_fishes[:n_fishes]
+    max_fishes = sorted_fishes[-n_fishes:][::-1]
+
+    print(f"The {n_fishes} Fishes with the MINIMUM Values in Principal Component #{principal_component_index}")
+    for n in min_fishes:
+        print_reconstruction_compare(pca_object, pca_input, transformed_fishes, n_components, n)
+
+    print(f"The {n_fishes} Fishes with the MAXIMUM Values in Principal Component #{principal_component_index}")
+    for n in max_fishes:
+        print_reconstruction_compare(pca_object, pca_input, transformed_fishes, n_components, n)
+
+
+def print_variance_plot(pca_object, n_components):
+    plt.bar(range(1, n_components + 1), pca_object.explained_variance_ratio_)
+    plt.title(f"Explained Variance by the {n_components} Principal Components")
+    text = [f"PC #{i}" for i in range(1, n_components + 1)]
+    plt.xticks(np.arange(n_components) + 1, text)
+    plt.show()
+
+
+def print_pc_wiggle_effects(pca_object, transformed_fishes, n_components, figsize):
+    mean_fish = np.zeros((n_components,))
+    fig, axes = plt.subplots(ncols=5, nrows=n_components)
+    fig.set_figheight(val=figsize[0])
+    fig.set_figwidth(val=figsize[1])
+    fig.subplots_adjust(wspace=0, hspace=0)
+    topText = ["-2 Std.Dev.", "-1 Std.Dev.", "Mean Fish", "+1 Std.Dev.", "+2 Std.Dev."]
+    for i, _ in enumerate(axes):
+        stdev_for_this_pc = statistics.stdev(transformed_fishes[:, i])
+        for j, _ in enumerate(axes[i]):
+            factor = (j - 2) * stdev_for_this_pc
+
+            modified_mean_fish = copy.copy(mean_fish)
+            modified_mean_fish[i] = factor
+            mod = pca_object.inverse_transform(modified_mean_fish)
+            mod = fishy.get_fish_on_canvas(xy_array=mod, canvas_size=(150, 150))
+            axes[i, j].imshow(mod, cmap="gray")
+
+            if i == 0:
+                axes[0, j].set_title(topText[j])
+
+            if j == 0:
+                axes[i, 0].set_ylabel(f"PC #{i+1}")
+                axes[i, 0].get_yaxis().set_ticks([])
+            else:
+                axes[i, j].get_yaxis().set_visible(False)
+
+            axes[i, j].get_xaxis().set_visible(False)
+
+    plt.show()
+
+
+def nice_pca_infos(pca_input, n_components):
+    # PCA-Objekt bauen
+    transformed_fishes, fish_pca = pca.transform_fishes(n_components=n_components)
+    # PCA(n_components=n_components)
+    # = fish_pca.fit_transform(pca_input)
+
+    # Varianz-Verteilungs-Plot
+    # TODO Farbig machen mit infos aus unserer halbsubjektiven Bewertung
+    print_variance_plot(fish_pca, n_components)
+
+    # Mean-Fisch wackeln lassen
+    goodFigsize = [3.7 * n_components, 18]
+    print_pc_wiggle_effects(n_components=n_components, pca_object=fish_pca, transformed_fishes=transformed_fishes,
+                            figsize=goodFigsize)
+
+    # Die extremsten Fische bezügliche einer Hauptkomponente printen lassen
+    print_min_max_fishes(pca_object=fish_pca,
+                         pca_input=pca_input,
+                         transformed_fishes=transformed_fishes,
+                         n_components=n_components,
+                         n_fishes=2,
+                         principal_component_index=0)
+
+    # Irgendwas mit Slidern
+
+    # return?
